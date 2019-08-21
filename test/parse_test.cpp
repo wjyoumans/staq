433461ea2336dab9461198f2271d4c1d325bef33
Matt Amy <matt.e.amy@gmail.com>
2019-08-21 15:04:58 -0400

    Importing qpp parser & lexical analysis

--- test/parse_test.cpp
+++ test/parse_test.cpp
@@ -1,39 +1,22 @@
-#define FMT_HEADER_ONLY = true
+#include <iostream>
+#include <string>
 
-#include "qasm/qasm.hpp"
-#include "qasm/visitors/ast_printer.hpp"
-#include "qasm/visitors/source_printer.hpp"
-#include "qasm/visitors/resource_estimator.hpp"
+#include "parser/position.h"
+#include "parser/token.h"
+#include "parser/lexer.h"
+#include "parser/preprocessor.h"
 
-#include <unordered_map>
-
-using namespace synthewareQ::qasm;
+using namespace synthewareQ::parser;
 
 int main(int argc, char** argv) {
 
   if (argc < 2) {
     std::cerr << "Input file not specified.\n";
+    return -1;
   }
 
-  auto program = synthewareQ::qasm::read_from_file(argv[1]);
-  if (program) {
-    std::cout << "Success!\nAST:\n";
-    ast_printer printer(std::cout);
-    printer.visit(*program);
-
-    std::cout << "\nUntransformed source:\n";
-    source_printer src(std::cout);
-    src.visit(*program);
-
-    std::cout << "\nResource estimates:\n";
-    resource_estimator res;
-    auto count = res.estimate(*program);
-    for (auto& [name, num] : count) {
-      std::cout << "  " << name << ": " << num << "\n";
-    }
-  } else {
-    std::cout << "Parsing of file \"" << argv[1] << "\" failed\n";
-  }
-
+  Preprocessor pp;
+  pp.add_target_file(argv[1]);
+  pp.print_all_tokens();
   return 1;
 }
