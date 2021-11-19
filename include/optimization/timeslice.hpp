6cc7eb9003ea78bd79f3b32537073457199e3805
wyoumans <wyoumans@mail.usf.edu>
2021-11-18 23:26:06 -0500

    initial commit of timeslicer optimization pass

--- include/optimization/timeslice.hpp
+++ include/optimization/timeslice.hpp
@@ -26,13 +26,14 @@
 
 /**
  * \file optimization/timeslice.hpp
- * \brief Gate reorganization optimization using commutativity rules
+ * \brief Gate reorganization using commutativity rules
  */
 
 #pragma once
 
 #include "qasmtools/ast/visitor.hpp"
 #include "qasmtools/ast/replacer.hpp"
+//#include "qasmtools/parser/position.hpp"
 
 #include <tuple>
 
@@ -42,7 +43,7 @@ namespace optimization {
 namespace ast = qasmtools::ast;
 
 /**
- * \brief Gate reorganization optimization using commutativity rules
+ * \brief Gate reorganization using commutativity rules
  */
 
 class Timeslicer final : public ast::Visitor {
@@ -87,7 +88,80 @@ class Timeslicer final : public ast::Visitor {
     /* Gates */
     // TODO: Special rules for UGates with parameters that match named gates (x gate etc.)?
     void visit(ast::UGate&) {}
-    void visit(ast::CNOTGate& gate) {}
+    void visit(ast::CNOTGate& gate) {
+        auto ctrl = gate.ctrl();
+        auto tgt = gate.tgt();
+
+        if (mergeable_) {
+            // loop through timeslices in reverse looking for best fit
+            int best = -1;
+            int uid;
+
+            for (int i = timeslices_.size() - 1; i >= 0; i--) {
+                // no conflict by default
+                int conflict = 0;
+
+                // conflict needs investigation
+                if (timeslices_[i].find(ctrl) != timeslices_[i].end() ||
+                        timeslices_[i].find(tgt) != timeslices_[i].end()) {
+                    
+                    // unresolveable conflict by default
+                    conflict = 1;
+
+                    auto [name1, args1, pos1, uid1] = timeslices_[i][ctrl];
+                    auto [name2, args2, pos2, uid2] = timeslices_[i][tgt];
+
+                    // gates commute, resolveable conflict
+                    if (uid1 == uid2 && name1 == "cx" && 
+                        args1 == std::vector<ast::VarAccess>({tgt, ctrl})) {
+                        conflict = -1;
+                    }
+                }
+
+                if (conflict == 0) { 
+                    best=i; 
+                } else if (conflict == 1) {
+                    break;
+                }
+                // if conflict == -1 we keep looking
+            }
+
+            // move gate to best timeslice
+            if (best >= 0) {
+                timeslices_[best][ctrl] = {"cx", {ctrl, tgt}, gate.pos(), gate.uid()};
+                timeslices_[best][tgt] = {"cx", {ctrl, tgt}, gate.pos(), gate.uid()};
+                
+                /* How to update the AST? This is an attempt to replace an arbitrary gate in the 
+                 * 'best' timeslice with itself plus the new CX gate.
+
+                auto [name, args, pos, uid] = std::begin(timeslices_[best])->second;
+               
+                std::list<ast::ptr<ast::Gate>> tmp;
+                tmp.emplace_back(
+                    new ast::CNOTGate(pos, ctrl, tgt));
+                tmp.emplace_back(std::move(gate));
+                
+                replacement_list_[gate.uid()] = std::move(std::list<ast::ptr<ast::Gate>>());
+                replacement_list_[uid] = std::move(tmp);
+                */
+                return;
+            }
+        }
+
+        // put gate in new timeslice
+        std::unordered_map<
+            ast::VarAccess, 
+            std::tuple<
+                std::string, 
+                std::vector<ast::VarAccess>, 
+                qasmtools::parser::Position, 
+                int>> temp = {
+            {ctrl, {"cx", {ctrl, tgt}, gate.pos(), gate.uid()}},
+            {tgt, {"cx", {ctrl, tgt}, gate.pos(), gate.uid()}},
+        };
+        timeslices_.push_back(temp);
+    }
+	
     void visit(ast::BarrierGate& gate) {}
     void visit(ast::DeclaredGate& gate) {}
 
@@ -105,15 +179,29 @@ class Timeslicer final : public ast::Visitor {
   private:
     config config_;
     bool mergeable_;
-    std::unordered_map<
+    std::vector<std::unordered_map<
         ast::VarAccess,
-        std::tuple<std::string, std::vector<ast::VarAccess>, int>>
-        last_;
+        std::tuple<std::string, std::vector<ast::VarAccess>, qasmtools::parser::Position, int>>>
+        timeslices_;
+    std::unordered_map<int, std::list<ast::ptr<ast::Gate>>> replacement_list_;
 
     void reset() {
-        last_.clear();
+        replacement_list_.clear();
+        timeslices_.clear();
         mergeable_ = true;
     }
+
+    /* reconstruct ptr<Gate> with something like this?
+    ast::ptr<ast::DeclaredGate> generate_cnot(int i, int j) {
+        parser::Position pos;
+        std::string name = "cx";
+        std::vector<ast::ptr<ast::Expr>> cargs;
+        std::vector<ast::VarAccess> qargs{(map_qubit_.find(i))->second,
+                                          (map_qubit_.find(j))->second};
+
+        return std::make_unique<ast::DeclaredGate>(pos, name, std::move(cargs),
+                                                   std::move(qargs));
+    }*/
 };
 
 inline void timeslice(ast::ASTNode& node) {
